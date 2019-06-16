d8a3f54ea971fb5e4df940e8c2dee34b0c9509d1
dbadb <dana@cannerycoders.com>
2019-06-15 18:17:02 -0700

    Starting to wire in now page (streaming).

--- www/js/ph/now.js
+++ www/js/ph/now.js
@@ -21,8 +21,40 @@ class Now extends PageHandler
     BuildPage(page, navextra, searchParams)
     {  
         app.setDateVisibility(false);
-        page.innerHTML = "";
+
+        // here we defeat the grid entirely
+        page.innerHTML = 
+        "<div style='grid-column-end:span 100;grid-row-end:span 100'>" +
+            "<div>"+
+                "<button type='button' id='streamStart'>Start Stream</button>" +
+                "<button type='button' id='streamStop'>Stop Stream</button>" +
+                "<button type='button' id='streamReset'>Reset Stream</button>" +
+                "<span id='streamStatus'>placeholder</span>" +
+            "</div>" +
+            "<canvas class='cameraViewImg' id='videocanvas'></canvas>" +
+        "</div>";
         navextra.innerHTML = "";
+        document.getElementById("streamStart").onclick = 
+                                    this._onStart.bind(this);
+        document.getElementById("streamStop").onclick = 
+                                    this._onStop.bind(this);
+        document.getElementById("streamReset").onclick = 
+                                    this._onReset.bind(this);
+    }
+
+    _onStart()
+    {
+        console.log("stream start");
+    }
+
+    _onStop()
+    {
+        console.log("stream stop");
+    }
+
+    _onReset()
+    {
+        console.log("stream reset");
     }
 }
 
